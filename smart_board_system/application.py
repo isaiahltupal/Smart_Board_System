8fb4bbeaf8b8359de8527eb0a800baf63c0eb900
isaiah <isaiah.jassen.l.tupal>
2020-04-25 19:23:53 +0800

    added the rectangle

--- smart_board_system/application.py
+++ smart_board_system/application.py
@@ -4,17 +4,22 @@ Author: Ding Bayeta and Isaiah Tupal
 This is the main python fail and its task is to manage the functions and processes.
 
 """
-
+from math import floor
 import cv2
 
 
 def main():
     video = cv2.VideoCapture(0)
     while True:
-        success,img = video.read()
-        cv2.imshow("Video Capture", img)
+        alert,frame = video.read()
+        computedPoints =floor(len(frame)*.25)
+
+        cv2.rectangle(frame,(0,0),(computedPoints,len(frame)),10,3)
+        cv2.imshow("Video Capture", frame)
         if cv2.waitKey(1) & 0xFF == ord('p'):
             break
+
+
 
 if __name__ == '__main__':
     main()
